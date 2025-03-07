5b31fd96fc40c7cb3b716524e8bcbdcc097cdc48
Vineet Chauhan <vineet.chd09@gmail.com>
2025-03-07 08:05:11 +0530

    Add index file for chat components

---
+++ src/components/chat/index.ts
@@ -0,0 +1,6 @@
+export { default as Chat } from './Chat';
+export { default as ChatInput } from './ChatInput';
+export { default as ChatWindow } from './ChatWindow';
+export { default as UserList } from './UserList';
+export { default as ChatHeader } from './ChatHeader';
+export { default as NotificationSystem } from './NotificationSystem';
