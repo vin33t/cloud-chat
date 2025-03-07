e7d51d0392cae38699c0eada247ddece286859d9
Vineet Chauhan <vineet.chd09@gmail.com>
2025-03-07 07:50:59 +0530

    Add remaining application files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chat from './components/chat/Chat';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
