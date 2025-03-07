15fba95f6247107f92fd5170ca5c5623e45a4624
Vineet Chauhan <vineet.chd09@gmail.com>
2025-03-07 08:04:59 +0530

    Fix App component to properly render Chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Chat from './components/chat/Chat';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Chat />
     </div>
   );
